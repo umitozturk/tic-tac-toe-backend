1625cf4dea0eb327c3e9f2d1db5f47b7a8884c3f
Ümit Öztürk <ozturkumit34@gmail.com>
2022-01-17 03:06:45 +0300

    feature(#8): add players controller and routes (#12)
    * handle some configurations
    
    * make rubocop happy!
    
    * #feature(#8): add players controller and routes

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class PlayerSerializer < ActiveModel::Serializer
   attributes :id, :name
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :players, only: %i(index show new create edit update destroy)
 end
